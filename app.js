5541166162e1ece952f725959bd7c6d2497161ec
kim <kim@eledia.com>
2025-07-20 16:35:41 +0800

    1st

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const users = require('./routes/user');
+const itemRoutes = require('./routes/item');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');  
+const chartRoutes = require('./routes/chart');
+const reviewRoutes = require('./routes/review');
+
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));       
+app.use('/images', express.static('images')); 
+
+app.use('/api/users', users);               
+app.use('/api/item', itemRoutes);           
+app.use('/api/category', categoryRoutes);
+app.use('/api/orders', orderRoutes);  
+app.use('/api/chart', chartRoutes); 
+app.use('/api/reviews',reviewRoutes);
+
+
+module.exports = app;
